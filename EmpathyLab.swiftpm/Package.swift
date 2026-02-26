598e058931f6a649567bc301aa797674a50816e3
bird <williams.lendjoungou@mail.mcgill.ca>
2026-02-25 23:25:09 -0500

    Structure of code implementation

---
+++ EmpathyLab.swiftpm/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "EmpathyLab",
+    platforms: [
+        .iOS("17.0")
+    ],
+    products: [
+        .executable(
+            name: "EmpathyLabApp",
+            targets: ["EmpathyLabApp"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "EmpathyLabApp",
+            dependencies: [],
+            path: "Sources/EmpathyLabApp",
+            resources: [
+                .process("../../Resources")
+            ]
+        )
+    ]
+)
---
+++ EmpathyLab.swiftpm/Sources/EmpathyLabApp/EmpathyLabApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct EmpathyLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppRootView()
+        }
+    }
+}
